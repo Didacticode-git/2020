3959a00b7f5aa9171ba658e0aca5d7ef94447c29
Didacticode-git <61479987+Didacticode-git@users.noreply.github.com>
2020-02-28 00:50:38 +0100

    Add files via upload

---
+++ BotDiscord-Python/main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+from discord.ext.commands.cooldowns import BucketType
+from random import randint
+token="NjgyNzMwNTcyMDUyOTU1MTc0.XlhQdQ.LUuyCGM2WHwKLOnkU0Q6tHHiOOc"
+Client=discord.Client()
+client=commands.Bot(command_prefix="didac!")
+
+@client.event
+async def on_ready():
+    print("Bot lancé !")
+
+@client.command(pass_context=True)
+async def roll(ctx, nb_max:int):
+    embed=discord.Embed(
+        title="Lancé de {}".format(ctx.message.author.name),
+        description="Résultat : {}".format(randint(1,nb_max))
+
+    )
+    embed.add_field(
+        name="Tu veux toi aussi lancer un dé ?",
+        value="Alors fait didac!roll <nombre maximum>",
+        inline=True
+    )
+    await client.send_message(ctx.message.author,embed=embed)
+    pass
+
+
+client.run(token)
